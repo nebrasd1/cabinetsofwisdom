a8fa68f0bdbd31719f8d5ac7fa30b2887df57850
Nebras D <nebrasd@proton.me>
2025-04-12 14:42:05 +0300

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "knowledge.ahmaddialdin.com",
+    baseUrl: "cabinetsofwisdom.com",
     ignorePatterns: [".smart-env", "private", "templates", ".stfolder", ".obsidian", ".trash", "Templates", "00 Meta/Excalidraw", "00 Meta/Dataviews", "00/Meta/Obsidian Guidance", "00 Meta/AI Prompts", "00 Meta/Templates", "Information/References/Conversations/", "Information/Fiction Ideas/", "Information/References/Websites/", "Information/References/PDFs/", "50 Personal/", "70 RPGs/", "Relations Testing", "smart-chats", "Context"],
     //defaultDateType: "created",
     defaultDateType: "modified", //date modified component
