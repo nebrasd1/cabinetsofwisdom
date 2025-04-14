f65413332603e4c633d552e6579319697b0e0857
Ahmad Dialdin <subcom.github.79@ishtirak.slmail.me>
2025-04-14 13:29:45 +0300

    Added sidebar to 404 page

--- quartz/plugins/emitters/404.tsx
+++ quartz/plugins/emitters/404.tsx
@@ -9,13 +9,27 @@ import { NotFound } from "../../components"
 import { defaultProcessedContent } from "../vfile"
 import { write } from "./helpers"
 import { i18n } from "../../i18n"
+import * as Component from "../../components"
+
 
 export const NotFoundPage: QuartzEmitterPlugin = () => {
   const opts: FullPageLayout = {
     ...sharedPageComponents,
     pageBody: NotFound(),
     beforeBody: [],
-    left: [],
+    left: [
+    Component.PageTitle(),
+    Component.MobileOnly(Component.Spacer()),
+    Component.Flex({
+      components: [
+        {
+          Component: Component.Search(),
+          grow: true,
+        },
+        { Component: Component.Darkmode() },
+      ],
+    }),
+  ],
     right: [],
   }
 
