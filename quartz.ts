2f6eb879d38089be22f7d746f45dc786ab81eb97
nebrasd1 <nebrasd@proton.me>
2026-06-06 18:59:01 +0300

    Add quartz-links-header plugin for top navigation

--- quartz.ts
+++ quartz.ts
@@ -1,4 +1,9 @@
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import * as ExternalPlugin from "./.quartz/plugins"
+
+ExternalPlugin.RecentNotes({
+    filter: (f) => f.slug!.startsWith("output/") && !f?.frontmatter?.index
+})
 
 const config = await loadQuartzConfig()
 export default config
